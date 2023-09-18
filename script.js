db3ab0349035a93c6d7eacf526a673d257ef8936
Vitor Silveira <vitor.silveiravtss@gmail.com>
2023-09-18 11:13:59 -0300

    smooth scroll done

--- script.js
+++ script.js
@@ -21,18 +21,43 @@ function initTabNav() {
 
 function initFAQAccordion() {
   const faqQuestions = document.querySelectorAll(".js-accordion dt");
+  faqQuestions[0].classList.add("activate-accordion");
+  faqQuestions[0].nextElementSibling.classList.add("activate-accordion");
 
-  if(faqQuestions.length){
+  if (faqQuestions.length) {
     function activeAccordion(event) {
-      event.currentTarget.classList.toggle("activate-accordion")
-      event.currentTarget.nextElementSibling.classList.toggle("activate-accordion")
+      event.currentTarget.classList.toggle("activate-accordion");
+      event.currentTarget.nextElementSibling.classList.toggle(
+        "activate-accordion"
+      );
     }
-  
+
     faqQuestions.forEach((question) => {
       question.addEventListener("click", activeAccordion);
     });
   }
 }
 
+function initSmoothScrolls() {
+  const internLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const itemHref = event.currentTarget.getAttribute("href");
+    const itemSection = document.querySelector(itemHref);
+    const top = itemSection.offsetTop;
+  
+    window.scrollTo({
+      top: top,
+      behavior: 'smooth'
+    });
+  }
+  
+  internLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
 initTabNav();
 initFAQAccordion();
+initSmoothScrolls();
